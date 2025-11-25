aa1da195d8e9c30617475009d9d24b6d7000626f
sabirtinayev <115555522+tmprl@users.noreply.github.com>
2025-11-25 21:52:49 +0400

    add transition when pressing the key

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,7 +1,17 @@
 window.addEventListener("keydown", function(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
     if(!audio) return;
-    audio.currentTime = 0;
-    audio.play()
+    audio.currentTime = 0;  //rewind the sounds to the start
+    audio.play();
+    key.classList.add("playing");
     console.log(audio)
 });
+
+function removeTransition(e) {
+    if(e.propertyName !== "transform") return;
+    this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
