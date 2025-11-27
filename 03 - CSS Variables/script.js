db210f6544a5804c4ba4fec95485be31ad65b39a
sabirtinayev <115555522+tmprl@users.noreply.github.com>
2025-11-27 20:52:57 +0400

    add property for all inputs, to change value

--- 03 - CSS Variables/script.js
+++ 03 - CSS Variables/script.js
@@ -2,7 +2,9 @@ const input = document.querySelectorAll(".controls input")
 
 
 function updateVariable() {
-    console.log(this.value);
+    const suffix = this.dataset.sizing || "";
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix) // changing from root/html every input 
 }
 
 input.forEach(input => input.addEventListener("change",updateVariable));
+input.forEach(input => input.addEventListener("mousemove",updateVariable));
