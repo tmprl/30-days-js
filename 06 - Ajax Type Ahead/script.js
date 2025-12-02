690604b6239997a0b00be7396fc72ab926ed75ae
sabirtinayev <115555522+tmprl@users.noreply.github.com>
2025-12-02 22:29:21 +0400

    create displayCities Function, use map

--- 06 - Ajax Type Ahead/script.js
+++ 06 - Ajax Type Ahead/script.js
@@ -14,11 +14,22 @@ function findMatches(wordToFind, cities) {
 }
 
 function displayCities() {
-    console.log(this.value);
+    const matchArray = findMatches(searchInput.value, cities);
+    const html = matchArray.map(place => {
+        const regex = new RegExp(this.value, 'gi');
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`)
+        return `
+        <li>
+            <span class = "name">${cityName}, ${place.state}</span>
+            <span class = "population">${place.population}</span>
+        </li>`;
+    }).join('');
+    suggestionInput.innerHTML = html;
 }
 
 const searchInput = document.querySelector('.search')
 const suggestionInput = document.querySelector('.suggestions')
 
-searchInput.addEventListener('change',findMatches)
+searchInput.addEventListener('change', displayCities)
+searchInput.addEventListener('keyup', displayCities)
 
