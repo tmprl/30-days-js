86a53600c6c841b2bab6868144a82b379b2699d4
sabirtinayev <115555522+tmprl@users.noreply.github.com>
2025-12-02 21:39:38 +0400

    fetch cities data, add findMatches function

--- 06 - Ajax Type Ahead/script.js
+++ 06 - Ajax Type Ahead/script.js
@@ -1,5 +1,24 @@
 const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
 
-const cities = [];
+let cities = [];
 
-fetch(endpoint);
+fetch(endpoint)
+      .then(blob => blob.json())
+      .then(data => cities = data)
+
+function findMatches(wordToFind, cities) {
+    return cities.filter(place => {
+        const regex = new RegExp(wordToFind, 'gi');
+        return place.city.match(regex) || place.state.match(regex);
+    });
+}
+
+function displayCities() {
+    console.log(this.value);
+}
+
+const searchInput = document.querySelector('.search')
+const suggestionInput = document.querySelector('.suggestions')
+
+searchInput.addEventListener('change',findMatches)
+
