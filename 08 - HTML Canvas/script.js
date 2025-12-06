a79909496f293ed5b72c4d4fce5460f8f3119382
sabirtinayev <115555522+tmprl@users.noreply.github.com>
2025-12-06 21:52:18 +0400

    add hue and lineWidth to draw function

--- 08 - HTML Canvas/script.js
+++ 08 - HTML Canvas/script.js
@@ -5,17 +5,46 @@ canvas.height = window.innerHeight;
 context.strokeStyle = '#BADA55';
 context.lineJoin = 'round';
 context.lineCap =  'round';
+context.lineWidth = 100;
+context.globalCompositeOperation = 'space-around'
 
 let isDrawing = false;
 let lastX = 0;
 let lastY = 0;
+let hue = 0;
+let direction = true;
 
 function draw(e) {
     if(!isDrawing) return;
     console.log(e)
+    context.strokeStyle = `hsl(${hue}, 100%, 50%)`
+    context.beginPath();
+    context.moveTo(lastX, lastY);
+    context.lineTo(e.offsetX, e.offsetY);
+    context.stroke();
+    lastX = e.offsetX
+    lastY = e.offsetY
+
+    hue++;
+    if(hue >= 360) {
+        hue = 0;
+    }
+    if(context.lineWidth >= 120 || context.lineWidth <= 1) {
+        direction = !direction;
+    }
+
+    if(direction) {
+        context.lineWidth++;
+    } else {
+        context.lineWidth--;
+    }
 }
 
 canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mousedown', (e) => {
+    isDrawing = true;
+    lastX = e.offsetX
+    lastY = e.offsetY
+});
 canvas.addEventListener('mouseup', () => isDrawing = false);
 canvas.addEventListener('mouseout', () => isDrawing = false);
