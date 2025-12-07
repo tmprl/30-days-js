3f39ec763ee991628cc36b815c2d6afacb7df463
sabirtinayev <115555522+tmprl@users.noreply.github.com>
2025-12-07 23:41:31 +0400

    select query of checkboxes input, add event listener to checBoxes

---
+++ 09 - Check Checkboxes/script.js
@@ -0,0 +1,7 @@
+const checkBoxes = document.querySelectorAll('.inbox input[type="checkbox"]')
+
+function handleCheck() {
+
+}
+
+checkBoxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
