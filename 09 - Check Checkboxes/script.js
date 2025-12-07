e1d95e7971af1d1f91caa576814531fdc1086c55
sabirtinayev <115555522+tmprl@users.noreply.github.com>
2025-12-07 23:49:38 +0400

    add function for handling checks when shift is pressed

--- 09 - Check Checkboxes/script.js
+++ 09 - Check Checkboxes/script.js
@@ -1,7 +1,22 @@
 const checkBoxes = document.querySelectorAll('.inbox input[type="checkbox"]')
 
-function handleCheck() {
+let lastChecked = false;
 
+function handleCheck(e) {
+
+    if(e.shiftKey && this.checked && lastChecked) {
+        let inBetween = false;
+
+        checkBoxes.forEach(box => {
+            if (box === this || box == lastChecked) {
+                inBetween = !inBetween;
+            }
+            if(inBetween) {
+                box.checked = true;
+            }
+        });
+    } 
+    lastChecked = this;
 }
 
 checkBoxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
